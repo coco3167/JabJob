38f34945e6107d5c9d73e5308ecfb4d5ad60a25c
coco3167 <corentinboblet@gmail.com>
2022-06-19 18:13:51 +0200

    Hope this works, Credit2.mp4

--- Assets/EndZone.cs
+++ Assets/EndZone.cs
@@ -1,55 +1,13 @@
-using _JabJob.Prefabs.Player.Scripts;
-using Acron0;
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class EndZone : MonoBehaviour
 {
-    public Image fadeImage;
-    public AudioSource carAudioSource;
-    public AudioSource audioSource;
-    
-    private bool _isEndZone = false;
-    private float _time;
-    private bool _finalAudioPlayed = false;
-
     public void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player"))
             return;
-        
-        _isEndZone = true;
-    }
 
-    private void Update()
-    {
-        if (!_isEndZone)
-            return;
-        
-        _time += Time.deltaTime;
-
-        _time = Mathf.Min(_time, 10f);
-        
-        float uiEasedTimeFadeOut = Easings.CubicEaseInOut(Mathf.Min(_time, 1f) / 1f);
-        
-        if (uiEasedTimeFadeOut > 0.75f)
-            MovementController.Instance.CanMove = false;
-        
-        float easedTimeCarAudioIdle = 1f - Easings.CubicEaseInOut(Mathf.Min(_time, 2f) / 2f);
-        fadeImage.color = new Color(0.1f, 0.1f, 0.1f, uiEasedTimeFadeOut);
-        carAudioSource.volume = easedTimeCarAudioIdle;
-
-        if (_time > 2f && !_finalAudioPlayed)
-        {
-            audioSource.Play();
-            _finalAudioPlayed = true;
-        }
-
-        if (_finalAudioPlayed && !audioSource.isPlaying)
-        {
-            SceneManager.LoadScene("_JabJob/Scenes/EndScene");
-        }
+        SceneManager.LoadScene("_JabJob/Scenes/EndScene");
     }
 }
---
+++ Assets/_JabJob/Scenes/EndScene/ExitGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using _JabJob.Prefabs.Input_Controller.Scripts;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Video;
+
+public class ExitGame : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("quit", 30f);
+    }
+
+    private void quit()
+    {
+        Application.Quit();
+    }
+}
